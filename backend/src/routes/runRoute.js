2decaf0f8f70f4510fdbbe78be6cb5145afe9548
Shreyash Dabhade <76619153+ShreyashDabhade@users.noreply.github.com>
2026-03-24 22:52:06 +0530

    Enhance logging in runRoute for debugging
    Add logging for compiler results and AST body.

--- backend/src/routes/runRoute.js
+++ backend/src/routes/runRoute.js
@@ -7,6 +7,12 @@ router.post("/run", (req, res) => {
   try {
     const { code = "" } = req.body || {};
     const result = runCompiler(code);
+    console.log(result);
+		console.log("\n\n");
+    for (let i = 0; i < 5; i++) {
+			console.log(result.ast.body[i]);
+		}
+		console.log("\n\n");
     res.json(result);
   } catch (error) {
     res.status(400).json({ error: error.message });
